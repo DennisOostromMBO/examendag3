140eeaab29ed4f02db138308486da3293842759c
mahdi2662 <madialkhalidi83@gmail.com>
2025-07-01 11:50:34 +0200

    heb de controler af van userstory 1

--- app/Http/Controllers/FoodParcelController.php
+++ app/Http/Controllers/FoodParcelController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\FoodParcel;
-use App\Models\Supplier;
 use Illuminate\Http\Request;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\View\View;
 
 class FoodParcelController extends Controller
 {
+    public function index(Request $request)
+    {
+        $eetwens = $request->input('eetwens');
+        $foodParcels = \DB::select('CALL Get_all_FoodParcels()');
 
+        if ($eetwens && $eetwens !== 'all') {
+            $foodParcels = array_filter($foodParcels, function($parcel) use ($eetwens) {
+                return isset($parcel->Eetwens) && $parcel->Eetwens === $eetwens;
+            });
+        }
+
+        return view('food-packages.index', compact('foodParcels', 'eetwens'));
+    }
 }
